d0e8e43ec2c56884e2444046be15d9898ec86dfc
Alex K <askrasn@gmail.com>
2021-05-25 18:09:44 -0700

    initial setup

---
+++ src/app/components/button/button.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.scss']
+})
+
+export class ButtonComponent implements OnInit {
+  @Input() text: string | undefined;
+  @Input() color: string | undefined;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
