240dd4ce787c48be81e2a161951d28afc11d00cd
Alex K <askrasn@gmail.com>
2021-05-25 19:12:35 -0700

    + add tasks

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,21 @@
+import { Task } from './Task_interface';
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        text: "Task one",
+        day: "May 31, 2021 at 3:30pm",
+        reminder: true
+    },
+    {
+        id: 2,
+        text: "Task two",
+        day: "July 31, 2021 at 1:10pm",
+        reminder: false
+    },
+    {
+        id: 3,
+        text: "Task three",
+        day: "June 1, 2021 at 4:30pm",
+        reminder: true
+    },
+];
